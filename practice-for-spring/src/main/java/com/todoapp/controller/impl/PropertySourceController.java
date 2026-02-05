7405d539d9099c013ecee2717c630e1849aa2c0d
aydog4nn <zephyrflaner@gmail.com>
2026-02-05 18:58:52 +0300

    added-configurationProperties

--- practice-for-spring/src/main/java/com/todoapp/controller/impl/PropertySourceController.java
+++ practice-for-spring/src/main/java/com/todoapp/controller/impl/PropertySourceController.java
@@ -2,10 +2,13 @@ package com.todoapp.controller.impl;
 
 import com.todoapp.configuration.DataSource;
 import com.todoapp.configuration.GlobalProperties;
+import com.todoapp.configuration.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/property")
@@ -17,13 +20,12 @@ public class PropertySourceController {
     @GetMapping("/datasource")
     public DataSource GetDataSource(){
 
-        DataSource dataSource = new DataSource();
-        dataSource.setUrl(globalProperties.getUrl());
-        dataSource.setUsername(globalProperties.getUsername());
-        dataSource.setPassword(globalProperties.getPassword());
-
-        return dataSource;
+      return null;
     }
 
+    @GetMapping(value = "/getServers")
+    public List<Server> getServers(){
+        return globalProperties.getServers();
+    }
 
 }
--- practice-for-spring/src/main/java/com/todoapp/starter/PracticeForSpringApplication.java
+++ practice-for-spring/src/main/java/com/todoapp/starter/PracticeForSpringApplication.java
@@ -1,8 +1,10 @@
 package com.todoapp.starter;
 
+import com.todoapp.configuration.GlobalProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.todoApp")
 @EnableJpaRepositories(basePackages = {"com.todoapp"})
 @SpringBootApplication
-@PropertySource(value = "classpath:app.properties")
+@EnableConfigurationProperties(value = GlobalProperties.class)
 public class PracticeForSpringApplication {
 
 	public static void main(String[] args) {
